02193d93e8ad21acdf44228c83b1d1ca5bb66ef7
Lynne Kostakis <lkostaki@chicagobooth.edu>
2017-07-18 01:52:11 +0000

    roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get("/flexible/square/:a_number", { :controller => "calculations", :action => "flex_square" })
+
+  get("/flexible/square_root/:a_number", { :controller => "calculations", :action => "flex_square_root" })
+  
+  get("/square/new", { :controller => "calculations", :action => "square_form" })  
+  
+  get("/square/results", { :controller => "calculations", :action => "process_square" }) 
+  
+  get("/square_root/new", { :controller => "calculations", :action => "square_root_form" })  
+  
+  get("/square_root/results", { :controller => "calculations", :action => "process_square_root" }) 
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
 end
